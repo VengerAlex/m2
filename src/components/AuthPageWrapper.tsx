3d7f276aacf5cd1f47622428700f232ec0989f50
VengerAlex <alexvenger90@gmail.com>
2022-10-22 00:41:54 +0300

    UPD: dasasd

--- src/components/AuthPageWrapper.tsx
+++ src/components/AuthPageWrapper.tsx
@@ -8,32 +8,7 @@ import Grid from '@mui/material/Grid';
 export const AuthPageWrapper = ({children}: {children: any}) => {
     return (
         <Grid container component="main" sx={{height: '100vh'}}>
-            <CssBaseline/>
-            <Grid
-                item
-                xs={false}
-                sm={4}
-                md={7}
-                sx={{
-                    backgroundImage: 'url(https://source.unsplash.com/random)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                <Box
-                    sx={{
-                        my: 8,
-                        mx: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    {children}
-                </Box>
-            </Grid>
+            adsads
         </Grid>
     );
 }
