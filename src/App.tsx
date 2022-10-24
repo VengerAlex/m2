7196770d3cb4810c796c8ce5504ffdf3eb76e32c
VengerAlex <alexvenger90@gmail.com>
2022-10-24 21:22:22 +0300

    UPD: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Box from '@mui/material/Box';
-import {Typography} from "@mui/material";
 import "./index.css"
+import {useState} from "react";
 
 const App = () => {
+    const [color, setColor] = useState("#ffffff");
+
     return (
-        <Box>
-            <Typography>Hello World</Typography>
+        <Box sx={{backgroundColor: "#ccc", height: "100vh", padding: "100px"}}>
+            <label htmlFor="color">
+                <div className="color" style={{backgroundColor: color}}>
+                    <input onChange={e => setColor(e.target.value)} type="color" id="color" className="input-color" />
+                </div>
+            </label>
         </Box>
     );
 };
